f41696489cb19e1b50c203e5985ad0bf15436463
DESKTOP-RTBA25E\kouki <mattibou415@gmail.com>
2022-05-05 22:23:12 +0900

    制限時間の実装

--- Assets/MyScripts/ActSceneMG.cs
+++ Assets/MyScripts/ActSceneMG.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ActSceneMG : MonoBehaviour
 {
+    //制限時間のセット
+    private float timer;
+    [SerializeField]
+    private TextMeshProUGUI timeUI;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = rouletteManager.timelimit;
+        timeUI.text = timer.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 制限時間との差を取得しつづける
+        timer -= Time.deltaTime;
+
+        if (timer >= 0)
+        {
+            // 文字列に変換して表示
+            timeUI.text = timer.ToString("0");
+        }
     }
 }
